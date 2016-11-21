c8fd72b1451710acf0360a1b5c68d6e9560c3851
Filipe Rodrigues <filiperdo@gmail.com>
2016-11-21 15:45:03 -0200

    Add ajustes no layout

--- controllers/index.php
+++ controllers/index.php
@@ -17,13 +17,12 @@ class Index extends Controller {
 
     public function produto( $id_product )
     {
-        require_once 'models/product_model.php';
+        /*require_once 'models/product_model.php';
         $this->view->product = new Product_Model();
-        $this->view->product->obterProduct( $id_product );
+        $this->view->product->obterProduct( $id_product );*/
 
-        $this->view->title = 'Notícias';
+        $this->view->title = 'Produto';
     	$this->view->render('header.site');
-        $this->view->render('index/slider');
     	$this->view->render('index/produto');
     	$this->view->render('footer.site');
     }
