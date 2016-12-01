70ced49cc67bb98fd3eea7dbec3c1af236d33e09
Filipe <filiperdo@gmail.com>
2016-11-30 22:52:39 -0200

    Ajuste no bg-index

--- views/index/index.php
+++ views/index/index.php
@@ -3,13 +3,13 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-offset-2 col-md-8">
-					<h1 class="wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s">Grandes momentos merecem ser eternizados com <span>uma miniatura 3D</span></h1>
+					<!--<h1 class="wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s">Grandes momentos merecem ser eternizados com <span>uma miniatura 3D</span></h1>-->
 					<div class="element">
 						<!--<div class="sub-element">Hello, this is Typed.js</div>
 						<div class="sub-element">Awesome Template is provided by templatemo.com website for everyone</div>
 						<div class="sub-element">Download, edit and apply this awesome template for your websites</div>-->
 					</div>
-					<a data-scroll href="#about" class="btn btn-default wow fadeInUp" data-wow-offset="50" data-wow-delay="0.6s">Comece agora</a>
+					<!--<a data-scroll href="#about" class="btn btn-default wow fadeInUp" data-wow-offset="50" data-wow-delay="0.6s">Comece agora</a>-->
 				</div>
 			</div>
 		</div>
